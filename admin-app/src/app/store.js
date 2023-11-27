9e670cc7c2f84641fd02964962edc2061cd81a87
Hoàng Mai <hgmaidg@gmail.com>
2023-11-27 20:18:27 +0700

    Product Category + Brand

--- admin-app/src/app/store.js
+++ admin-app/src/app/store.js
@@ -2,10 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
 import customerReducer from "../features/customers/customerSlice";
 import productReducer from "../features/product/productSlice";
+import brandReducer from "../features/brand/brandSlice";
+import pCategoryReducer from "../features/pcategory/pcategorySlice";
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     customer: customerReducer,
     product: productReducer,
+    brand: brandReducer,
+    pCategory: pCategoryReducer,
   },
 });
