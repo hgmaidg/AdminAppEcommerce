dda104c7cbe05fde3dbd5df563d9470ee5b0251d
Hoàng Mai <hgmaidg@gmail.com>
2023-11-27 20:31:49 +0700

    Color + Blog

--- admin-app/src/app/store.js
+++ admin-app/src/app/store.js
@@ -4,6 +4,8 @@ import customerReducer from "../features/customers/customerSlice";
 import productReducer from "../features/product/productSlice";
 import brandReducer from "../features/brand/brandSlice";
 import pCategoryReducer from "../features/pcategory/pcategorySlice";
+import colorReducer from "../features/color/colorSlice";
+import blogReducer from "../features/blogs/blogSlice";
 export const store = configureStore({
   reducer: {
     auth: authReducer,
@@ -11,5 +13,7 @@ export const store = configureStore({
     product: productReducer,
     brand: brandReducer,
     pCategory: pCategoryReducer,
+    color: colorReducer,
+    blogs: blogReducer,
   },
 });
