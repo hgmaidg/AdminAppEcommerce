8df2a2c3ea05d1636e0e8e1bdb087326197f9831
Hoàng Mai <hgmaidg@gmail.com>
2023-11-27 16:02:29 +0700

    Revert "Admin Login"
    This reverts commit a4e87a2ea8742489409e5452aa938ec348c307c9.

--- admin-app/src/pages/Login.js
+++ admin-app/src/pages/Login.js
@@ -1,89 +1,28 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import CustomInput from "../components/CustomInput";
-import { useFormik } from "formik";
-import * as Yup from "yup";
-import { useDispatch, useSelector } from "react-redux";
-import { login } from "../features/auth/authSlice";
-import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  let schema = Yup.object().shape({
-    email: Yup.string()
-      .email("Email should be valid.")
-      .required("Email is required."),
-    password: Yup.string().required("Password is required."),
-  });
-  const formik = useFormik({
-    initialValues: {
-      email: "",
-      password: "",
-    },
-    validationSchema: schema,
-    onSubmit: (values) => {
-      dispatch(login(values));
-      alert(JSON.stringify(values, null, 2));
-    },
-  });
-  const { user, isLoading, isError, isSuccess, message } = useSelector(
-    (state) => state.auth
-  );
-  useEffect(() => {
-    if (!user === null || isSuccess) {
-      navigate("admin");
-    } else {
-      if (isError) {
-        alert(message);
-      }
-    }
-  }, [user, isLoading, isError, isSuccess, message]);
   return (
     <div className="py-5" style={{ background: "#563d7c", minHeight: "100vh" }}>
       <br /> <br /> <br /> <br />
       <div className="my-5 w-25 bg-white rounded-3 mx-auto p-4">
         <h3 className="text-center">Login</h3>
         <p className="text-center">Login to your account to continue</p>
-        <form action="" onSubmit={formik.handleSubmit}>
-          <CustomInput
-            type="text"
-            name="email"
-            label="Email Address"
-            id="email"
-            val={formik.values.email}
-            onCh={formik.handleChange("email")}
-            onBl={formik.handleChange("email")}
-          />
-          <div className="error">
-            {formik.touched.email && formik.errors.email ? (
-              <div>{formik.errors.email}</div>
-            ) : null}
-          </div>
-          <CustomInput
-            type="password"
-            name="password"
-            label="Password"
-            id="pass"
-            val={formik.values.password}
-            onCh={formik.handleChange("password")}
-            onBl={formik.handleChange("password")}
-          />
-          <div className="error">
-            {formik.touched.password && formik.errors.password ? (
-              <div>{formik.errors.password}</div>
-            ) : null}
-          </div>
+        <form action="">
+          <CustomInput type="text" label="Email Address" id="email" />
+          <CustomInput type="password" label="Password" id="pass" />
           <div className="mb-3 text-end">
             <Link to="/forgot-password">Forgot Password?</Link>
           </div>
-          <button
+          <Link
+            to="/admin"
             className="border-0 px-3 py-2 text-white fw-bold w-100 text-center text-decoration-none fs-5"
             style={{ background: "#f8c03e" }}
             type="submit"
           >
             Login
-          </button>
+          </Link>
         </form>
       </div>
     </div>
