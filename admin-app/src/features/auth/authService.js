cd2d4eac83975ccaf342afd9cdcfb9294c1efa5d
Hoàng Mai <hgmaidg@gmail.com>
2023-12-02 22:16:53 +0700

    Fix Auth

--- admin-app/src/features/auth/authService.js
+++ admin-app/src/features/auth/authService.js
@@ -1,27 +1,73 @@
+// import axios from "axios";
+// import { base_url } from "../../utils/base_url";
+
+// const login = async (userData) => {
+//   const reponse = await axios.post(`${base_url}user/login`, userData);
+//   if (reponse.data) {
+//     localStorage.setItem("user", JSON.stringify(reponse.data));
+//   }
+//   return reponse.data;
+// };
+// const getOrders = async () => {
+//   const response = await axios.get(`${base_url}user/getallorders`, config);
+
+//   return response.data;
+// };
+// const getTokenFromLocalStorage = localStorage.getItem("user")
+//   ? JSON.parse(localStorage.getItem("user"))
+//   : null;
+// const config = {
+//   headers: {
+//     Authorization: `Bearer ${getTokenFromLocalStorage.token}`,
+//     Accept: "application/json",
+//   },
+// };
+// const getOrder = async (id) => {
+//   const response = await axios.post(
+//     `${base_url}user/getorderbyuser/${id}`,
+//     "",
+//     config
+//   );
+//   return response.data;
+// };
+
+// const authService = {
+//   login,
+//   getOrders,
+//   getOrder,
+// };
+
+// export default authService;
+
 import axios from "axios";
 import { base_url } from "../../utils/base_url";
 
 const getTokenFromLocalStorage = localStorage.getItem("user")
   ? JSON.parse(localStorage.getItem("user"))
   : null;
+
 const config = {
   headers: {
-    Authorization: `Bearer ${getTokenFromLocalStorage.token}`,
+    Authorization: getTokenFromLocalStorage
+      ? `Bearer ${getTokenFromLocalStorage.token}`
+      : null,
     Accept: "application/json",
   },
 };
-const login = async (userData) => {
-  const reponse = await axios.post(`${base_url}user/login`, userData);
-  if (reponse.data) {
-    localStorage.setItem("user", JSON.stringify(reponse.data));
-  }
-  return reponse.data;
-};
-const getOrders = async () => {
-  const response = await axios.get(`${base_url}user/getallorders`, config);
 
+const login = async (userData) => {
+  const response = await axios.post(`${base_url}user/login`, userData);
+  if (response.data) {
+    localStorage.setItem("user", JSON.stringify(response.data));
+  }
   return response.data;
 };
+
+const getOrders = async () => {
+  const response = await axios.get(`${base_url}user/getallorders`, config);
+  return response.data;
+};
+
 const getOrder = async (id) => {
   const response = await axios.post(
     `${base_url}user/getorderbyuser/${id}`,
